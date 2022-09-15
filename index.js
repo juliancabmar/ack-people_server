4cbc876d2f4e021bb7d5bad0da251b4ad0e31aca
Julian Cabrera <a.j.c.marce@gmail.com>
2022-09-15 05:57:22 -0300

    app running whit enviroment variables

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
-const { MONGODB } = require('./config');
+const MONGODB = process.env.MONGODB;
 
 const pubsub = new PubSub();
 
